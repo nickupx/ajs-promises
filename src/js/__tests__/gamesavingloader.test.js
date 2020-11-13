ac84be592decd09b607b87210b7f49b4948ff296
nickupx <nick.upsky@gmail.com>
2020-11-13 14:35:32 +0200

    test done()

--- src/js/__tests__/gamesavingloader.test.js
+++ src/js/__tests__/gamesavingloader.test.js
@@ -2,8 +2,9 @@ import GameSavingLoader from '../gamesavingloader'
 
 const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}'
 
-test('should return', () => {
+test('should return', (done) => {
   GameSavingLoader.load().then((received) => {
-    expect(received).toEqual(expected)
+    expect(received).toBe(expected)
   }).catch((error) => error)
+  done()
 })
